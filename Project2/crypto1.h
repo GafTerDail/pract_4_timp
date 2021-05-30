c95ccab62a099a57e3cadd3abd112c06aeb49d65
GafTerDail <81242177+GafTerDail@users.noreply.github.com>
2021-05-30 21:41:24 +0300

    Update crypto1.h

--- Project2/crypto1.h
+++ Project2/crypto1.h
@@ -1,36 +1,75 @@
 
 #pragma once
 //for generate IV and Key
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\osrng.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\osrng.h"
 //using CryptoPP::AutoSeededRandomPool;
 
 #include <string>
 //using std::string;
 
 //for exceptions
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\cryptlib.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\cryptlib.h"
 //using CryptoPP::Exception;
 
 //for hex viewing
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\hex.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\hex.h"
 //using CryptoPP::HexDecoder;
 //using CryptoPP::HexEncoder;
 
 //converter input into cipher
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\filters.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\filters.h"
 //using CryptoPP::StringSink;
 //using CryptoPP::StreamTransformation;
 //using CryptoPP::StreamTransformationFilter;
 
 //add AES
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\aes.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\aes.h"
 //using CryptoPP::AES;
 
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\modes.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\modes.h"
 //using CryptoPP::CBC_Mode;
 
-#include "C:\Users\�����\Downloads\cryptopp-CRYPTOPP_8_5_0\md5.h"
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\md5.h"
 //using CryptoPP::MD5;
+
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\files.h"
+
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\des.h"
+
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\modes.h"
+
+#include "C:\Users\Артем\Downloads\cryptopp-CRYPTOPP_8_5_0\pwdbased.h"
+using namespace std;
+using namespace CryptoPP;
+class AES_CRYP
+{
+private:
+    string filein;
+    string fileout;
+    string psw;
+    string salt = "Я помню чудное мгновенье";
+public:
+    AES_CRYP() = delete;
+    AES_CRYP(const string& input, const string& output, const string& pass) : filein(input), fileout(output), psw(pass) {};
+    bool encrypt();
+    bool decrypt();
+};
+
+class DES_CRYP
+{
+private:
+    string filein;
+    string fileout;
+    string psw;
+    string salt = "Я помню чудное мгновенье";
+public:
+    DES_CRYP() = delete;
+    DES_CRYP(const string& input, const string& output, const string& pass) : filein(input), fileout(output), psw(pass) {};
+    bool encrypt();
+    bool decrypt();
+};
+
+
 
 
 
